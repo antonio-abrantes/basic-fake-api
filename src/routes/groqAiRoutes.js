3f7baf7a6320411d2ffc07f46b3edab97258cabe
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2024-08-27 18:58:47 -0300

    fix groq request 2

--- src/routes/groqAiRoutes.js
+++ src/routes/groqAiRoutes.js
@@ -51,7 +51,7 @@ router.post("/completions", authMiddleware, async (req, res) => {
   try {
     const completion = await retryRequest();
 
-    console.log({ name: "GroqResult", command, data: completion });
+    console.log({ name: "GroqResult", data: completion });
 
     const result = completion.choices[0]?.message?.content || "";
     const cleanedJsonString = result.replace(/```json|```/g, "").trim();
